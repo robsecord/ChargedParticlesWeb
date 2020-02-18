8a393a6d9430b58988893d8159a45a0514ef780e
xeroblood <xeroblood@owntheday.io>
2020-02-17 23:19:47 -0500

    Updates to theme Updates to manage page

--- src/app/blockchain/transactions.js
+++ src/app/blockchain/transactions.js
@@ -4,6 +4,10 @@ import { reactLocalStorage } from 'reactjs-localstorage';
 import * as _ from 'lodash';
 
 // App Components
+import {
+    ChargedParticles,
+    ChargedParticlesEscrow
+} from '../blockchain/contracts';
 import { Helpers } from '../../utils/helpers';
 import { GLOBALS } from '../../utils/globals';
 
@@ -13,11 +17,53 @@ import { searchTransactionEvent } from './queries/SearchTransactionEvent';
 
 // Transaction Events
 const transactionEventMap = {
-    'CREATE_PARTICLE_TYPE': {
-        eventName: 'ParticleTypeCreated',
-        method: 'ParticleTypeCreated(uint256,string,bool,bool,string,uint256)',
-        hash: '0xc201e7d2252eddeae969c897081b09a2442f097cb2b71d5257d1b22b26f265da',
-    }
+    // 'CREATE_PARTICLE_TYPE': {
+    //     eventName: 'ParticleTypeCreated',
+    //     method: 'ParticleTypeCreated(uint256,string,bool,bool,string,uint256,uint16)',
+    //     hash: '0xf21712078d07c8f6113360cff723aa378a66ea4fbf84b7f963aaaf5aabe999c0',
+    // },
+
+    'UPDATE_PARTICLE_TYPE': {   // find latest in logs for full record
+        contract    : ChargedParticles,
+        eventName   : 'ParticleTypeUpdated',
+        method      : 'ParticleTypeUpdated(uint256,string,bool,bool,string,uint256,uint16)',
+        hash        : '0xc428215361d42d56e92ee4eab4f93562276ccfce64238f55f138d86f97ea22d2',
+    },
+
+    'MINT_PARTICLE': {
+        contract    : ChargedParticles,
+        eventName   : 'ParticleMinted',
+        method      : 'ParticleMinted(uint256,uint256,string)',
+        hash        : '0x72b3b853f3f2ac9829ae716e6e9a4be69b8eb03f54e331e72004bf4f16e0f622',
+    },
+
+    'BURN_PARTICLE': {
+        contract    : ChargedParticles,
+        eventName   : 'ParticleBurned',
+        method      : 'ParticleBurned(uint256,uint256)',
+        hash        : '0x06cbc21e86440195808ffdb1a12a5fb80b27daa8686e0270b784b1a84b4dec51',
+    },
+
+    'ENERGIZE_PARTICLE': {
+        contract    : ChargedParticlesEscrow,
+        eventName   : 'EnergizedParticle',
+        method      : 'EnergizedParticle(address,uint256,bytes16,uint256,uint256)',
+        hash        : '0x86bc5fbc42fef15a4ea86854606b3656142f755038ff4758177d34ff0dab948e',
+    },
+
+    'DISCHARGE_PARTICLE': {
+        contract    : ChargedParticlesEscrow,
+        eventName   : 'DischargedParticle',
+        method      : 'DischargedParticle(address,uint256,address,bytes16,uint256,uint256)',
+        hash        : '0x2e9c971780e3865718eb2e4c20e3f5fda964d6e023641a0008212b53999cca1d',
+    },
+
+    'RELEASE_PARTICLE': {
+        contract    : ChargedParticlesEscrow,
+        eventName   : 'ReleasedParticle',
+        method      : 'ReleasedParticle(address,uint256,address,bytes16,uint256)',
+        hash        : '0x562bf32d1b1932a57a7e89b5ef87929b9b93b912bda6144ed92270744d6f1774',
+    },
 };
 
 class Transactions {
@@ -146,9 +192,12 @@ class Transactions {
     async searchTransactionsByEvent({eventId, owner}) {
         this.txDispatch({type: 'BEGIN_SEARCH', payload: {}});
 
+        const contract = transactionEventMap[eventId].contract.instance();
+        const contractAddress = _.toLower(contract.getAddress());
         const eventName = transactionEventMap[eventId].eventName;
         const methodHash = transactionEventMap[eventId].hash;
-        const query = `signer:${_.toLower(owner)} topic.0:${methodHash}`;
+        const query = `address: ${contractAddress} signer:${_.toLower(owner)} topic.0:${methodHash}`;
+        console.log('query', query);
 
         const response = await this.client.graphql(searchTransactionEvent, {
             variables: {
@@ -189,14 +238,7 @@ class Transactions {
 
                 // Get Decoded Transaction Data
                 const decoded = Helpers.decodeLog({eventName, logEntry});
-                searchTransactions.push({
-                    particleTypeId  : decoded._particleTypeId,
-                    uri             : decoded._uri,
-                    isNF            : decoded._isNF,
-                    isPrivate       : decoded._isPrivate,
-                    assetPairId     : decoded._assetPairId,
-                    maxSupply       : decoded._maxSupply,
-                });
+                searchTransactions.push(decoded);
             });
         });
 
--- src/app/components/ConnectionWarning.js
+++ src/app/components/ConnectionWarning.js
@@ -1,9 +1,11 @@
 // Frameworks
 import React, { useContext } from 'react';
+import UseAnimations from 'react-useanimations';
 import * as _ from 'lodash';
 
-// Rimble UI
-import { Flash } from 'rimble-ui';
+// Material UI
+import Alert from '@material-ui/lab/Alert';
+import Box from '@material-ui/core/Box';
 
 // Data Context for State
 import { RootContext } from '../stores/root.store';
@@ -18,9 +20,15 @@ function ConnectionWarning() {
     }
 
     return (
-        <Flash mt={2} mb={4} variant="danger">
-            {connectionWarning}
-        </Flash>
+        <Box mb={3}>
+            <Alert
+                variant="outlined"
+                severity="error"
+                icon={<UseAnimations animationKey="alertCircle" size={24} />}
+            >
+                {connectionWarning}
+            </Alert>
+        </Box>
     );
 }
 
